191de174a89d0d044b8e81a256d51252cbf343ed
Hamza Rana <33406161+hamzarana07@users.noreply.github.com>
2024-03-24 03:55:47 +0500

    Updated free-proxy-list.net code

--- proxies.py
+++ proxies.py
@@ -8,12 +8,16 @@ a = re.finditer(regex, test_str, re.MULTILINE)
 with open("proxies_list.txt", 'w') as file:
     for i in a:
        print(i.group(),file=file)
-#==========================================================
-
-c = requests.get("https://free-proxy-list.net/")
-soup = BeautifulSoup(c.content, 'html.parser')
-z = soup.find('textarea').get_text()
-x = re.findall(regex, z)
+        
+d = requests.get("https://free-proxy-list.net/")
+soup = BeautifulSoup(d.content, 'html.parser')
+td_elements = soup.select('.fpl-list .table tbody tr td')
+ips = []
+ports = []
+for j in range(0, len(td_elements), 8):
+    ips.append(td_elements[j].text.strip())
+    ports.append(td_elements[j + 1].text.strip())
 with open("proxies_list.txt", "a") as myfile:
-    for i in x:
-        print(i, file=myfile)
+    for ip, port in zip(ips, ports):
+        proxy = f"{ip}:{port}"
+        print(proxy, file=myfile)
